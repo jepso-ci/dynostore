b52a00854a385c10b3ac5b09fd0dcf6f829440aa
Forbes Lindesay <fpfl2@cam.ac.uk>
2012-12-31 16:25:17 +0000

    Handle completing builds
    Also supports gracefully handling multiple concurrent builds on the same
    repo

--- README.md
+++ README.md
@@ -2,16 +2,120 @@
 
   Dynamo db wrapper for the jepso-ci schema
 
-## Repositories
+## Sample Usage
 
-  id= {userID, repoID}
-  attr= {latestBuild, currentBuildInProgress?}
+```javascript
+var u = 'api';
+var r = 'foo';
+var t = 'master';
+createBuild(u, r, t, ['BrowserA', 'BrowserB'])
+  .then(function (buildID) {
+    console.log('buildID=' + buildID);
+    function logStatus(v) {
+      return getRepo(u, r)
+        .then(log)
+        .then(function () {
+          return getBuild(u, r, buildID);
+        })
+        .then(log)
+        .then(function () {
+          return v;
+        });
+    }
+    return logStatus()
+      .then(function () {
+        console.log("updateBuild(u, r, buildID, 'BrowserA', 'started', false)");
+        return updateBuild(u, r, buildID, 'BrowserA', 'started', false);
+      })
+      .then(logStatus)
+      .then(function () {
+        console.log("updateBuild(u, r, buildID, 'BrowserB', 'started', false)");
+        return updateBuild(u, r, buildID, 'BrowserB', 'started', false);
+      })
+      .then(logStatus)
+      .then(function () {
+        console.log("updateBuild(u, r, buildID, 'BrowserA', 'finished', true)");
+        return updateBuild(u, r, buildID, 'BrowserA', 'finished', true);
+      })
+      .then(logStatus)
+      .then(function () {
+        console.log("updateBuild(u, r, buildID, 'BrowserB', 'finished', true)");
+        return updateBuild(u, r, buildID, 'BrowserB', 'finished', true);
+      })
+      .then(logStatus)
+  }).done()
+function log(v) {
+  console.log(v);
+  return v;
+}
+```
 
-## Builds
+Sample Output:
 
-  id= {repoPath, hash}
-  attr= {chrome, firefox ....}
-
-  Each browser can be:
-
-  {'pass', 'fail', 'fail-n.n.n', 'pending-n.n.n', 'queued'}
+```
+buildID=1
+{ user: 'api',
+  repo: 'foo',
+  currentBuildInProgress: '1',
+  latestBuild: null }
+{ tag: 'master',
+  repoID: 'api/foo',
+  'BrowserA-pending': true,
+  buildID: 1,
+  pending: 2,
+  BrowserA: 'queued',
+  'BrowserB-pending': true,
+  BrowserB: 'queued' }
+updateBuild(u, r, buildID, 'BrowserA', 'started', false)
+{ user: 'api',
+  repo: 'foo',
+  currentBuildInProgress: '1',
+  latestBuild: null }
+{ tag: 'master',
+  repoID: 'api/foo',
+  'BrowserA-pending': true,
+  buildID: 1,
+  pending: 2,
+  BrowserA: 'started',
+  'BrowserB-pending': true,
+  BrowserB: 'queued' }
+updateBuild(u, r, buildID, 'BrowserB', 'started', false)
+{ user: 'api',
+  repo: 'foo',
+  currentBuildInProgress: '1',
+  latestBuild: null }
+{ tag: 'master',
+  repoID: 'api/foo',
+  'BrowserA-pending': true,
+  buildID: 1,
+  pending: 2,
+  BrowserA: 'started',
+  'BrowserB-pending': true,
+  BrowserB: 'started' }
+updateBuild(u, r, buildID, 'BrowserA', 'finished', true)
+{ user: 'api',
+  repo: 'foo',
+  currentBuildInProgress: '1',
+  latestBuild: null }
+{ tag: 'master',
+  repoID: 'api/foo',
+  'BrowserA-pending': false,
+  buildID: 1,
+  pending: 1,
+  BrowserA: 'finished',
+  'BrowserB-pending': true,
+  BrowserB: 'started' }
+updateBuild(u, r, buildID, 'BrowserB', 'finished', true)
+{ user: 'api',
+  repo: 'foo',
+  currentBuildInProgress: null,
+  latestBuild: '1' }
+{ tag: 'master',
+  repoID: 'api/foo',
+  'BrowserA-pending': false,
+  buildID: 1,
+  pending: 0,
+  BrowserA: 'finished',
+  'BrowserB-pending': false,
+  BrowserB: 'finished' }
+```
--- index.js
+++ index.js
@@ -31,30 +31,46 @@ aws.config.update({region: 'us-east-1'});
 var db = new aws.DynamoDB({sslEnabled: true}).client;
 exports.createTables = createTables;
 function createTables() {
-  var reposTable = fix(db.createTable({
-    TableName: 'repositories',
-    KeySchema: {
-      HashKeyElement: { AttributeName: 'user', AttributeType: 'S'},
-      RangeKeyElement: { AttributeName: 'repo', AttributeType: 'S'},
+  function createTable(name, keys, provisioning) {
+    return fix(db.createTable({
+      TableName: name,
+      KeySchema: {
+        HashKeyElement: { AttributeName: keys.hash.name, AttributeType: keys.hash.type},
+        RangeKeyElement: { AttributeName: keys.range.name, AttributeType: keys.range.type}
+      },
+      ProvisionedThroughput: {
+        ReadCapacityUnits: provisioning.read,
+        WriteCapacityUnits: provisioning.write
+      }
+    }))
+    .then(null, function (err) {
+      //ignore if table already exists
+      //requires care when changing schema
+      if (err.name === 'AWS:ResourceInUseException') return;
+      throw err;
+    });
+  }
+  var reposTable = createTable('repositories',
+    {
+      hash: {name: 'user', type: 'S'},
+      range: {name: 'repo', type: 'S'}
     },
-    ProvisionedThroughput: {
-      ReadCapacityUnits: 2,
-      WriteCapacityUnits: 1
-    }
-  }));
-  var buildsTable = fix(db.createTable({
-    TableName: 'builds',
-    KeySchema: {
-      HashKeyElement: { AttributeName: 'repoID', AttributeType: 'S'},
-      RangeKeyElement: { AttributeName: 'buildID', AttributeType: 'S'},
+    {
+      read: 2,
+      write: 1
+    });
+  var buildsTable = createTable('builds',
+    {
+      hash: {name: 'repoID', type: 'S'},
+      range: {name: 'buildID', type: 'N'}
     },
-    ProvisionedThroughput: {
-      ReadCapacityUnits: 8,
-      WriteCapacityUnits: 4
-    }
-  }));
-  reposTable.then(function () { return buildsTable; });
+    {
+      read: 8,
+      write: 4
+    });
+  return reposTable.then(function () { return buildsTable; }).then(function(){});
 }
+
 function listRepos(user) {
   
 }
@@ -70,23 +86,10 @@ function getRepo(user, repo) {
     return {
       user: data.Item.user.S,
       repo: data.Item.repo.S,
-      currentBuildInProgress: data.Item.currentBuildInProgress ? data.Item.currentBuildInProgress.S : null,
-      latestBuild: data.Item.latestBuild ? data.Item.latestBuild.S : null
+      currentBuild: data.Item.currentBuild ? data.Item.currentBuild.N : null,
+      latestBuild: data.Item.latestBuild ? data.Item.latestBuild.N : null
     }
   });
-}
-exports.setRepoBuildInProgress = setRepoBuildInProgress;
-function setRepoBuildInProgress(user, repo, build) {
-  return fix(db.updateItem({
-    TableName: 'repositories',
-    Key: {
-      HashKeyElement: { S: user },
-      RangeKeyElement: { S: repo }
-     },
-    AttributeUpdates: {
-      currentBuildInProgress: { Value: {S: build},  }
-    }
-  }));
 }
 exports.setRepoBuildComplete = setRepoBuildComplete;
 function setRepoBuildComplete(user, repo, build) {
@@ -105,20 +108,26 @@ function listBuilds(user, repo) {
 
 }
 exports.getBuild = getBuild;
-function getBuild(user, repo, tag) {
+function getBuild(user, repo, buildID) {
   return fix(db.getItem({
     TableName: 'builds',
     Key: {
       HashKeyElement: { S: user + '/' + repo },
-      RangeKeyElement: { S: tag }
+      RangeKeyElement: { N: '' + buildID }
     }
   })).then(function (data) {
     data = data.Item;
     var res = {};
     Object.keys(data)
       .forEach(function (key) {
-        if (typeof data[key] === 'object' && typeof data[key].S === 'string') {
-          res[key] = data[key].S;
+        if (typeof data[key] === 'object') {
+          if(typeof data[key].S === 'string') {
+            res[key] = data[key].S;
+          } else if (key === 'buildID' || key === 'pending') {
+            res[key] = +data[key].N;
+          } else if (typeof data[key].N === 'string') {
+            res[key] = data[key].N === '1' ? true : (data[key].N === '0' ? false : data[key].N);
+          }
         }
       });
     return res;
@@ -126,33 +135,100 @@ function getBuild(user, repo, tag) {
 }
 exports.createBuild = createBuild;
 function createBuild(user, repo, tag, browsers) {
-  var item = {
-      repoID: {S: user + '/' + repo},
-      buildID: {S: tag},
-      state: {S: 'pending'}
-    };
-  for (var i = 0; i < browsers.length; i++) {
-    item[browsers[i]] = {S: 'queued'}
-  }
-  return fix(db.putItem({
-    TableName: 'builds',
-    Item: item
-  }));
+  return fix(db.updateItem({
+    TableName: 'repositories',
+    Key: {
+      HashKeyElement: { S: user },
+      RangeKeyElement: { S: repo }
+     },
+    AttributeUpdates: {
+      currentBuild: { Value: {N: '1'}, Action: 'ADD' }
+    },
+    ReturnValues: 'UPDATED_NEW'
+  }))
+    .then(function (res) {
+      var buildID = res.Attributes.currentBuild.N;
+      var item = {
+          repoID: {S: user + '/' + repo},
+          buildID: {N: buildID},
+          tag: {S: tag},
+          pending: {N: '' + browsers.length}
+        };
+      for (var i = 0; i < browsers.length; i++) {
+        item[browsers[i]] = {S: 'queued'}
+        item[browsers[i] + '-pending'] = {N: '1'};
+      }
+      return fix(db.putItem({
+        TableName: 'builds',
+        Item: item
+      }))
+      .then(function () {
+        return buildID;
+      });
+    });
 }
 exports.updateBuild = updateBuild;
-function updateBuild(user, repo, tag, browser, status) {
+function updateBuild(user, repo, buildID, browser, status, finished) {
+  var expected = {};
+  expected[browser + '-pending'] = {Value: {N: '1'}};
   var update = {};
   update[browser] = {Value: {S: status}};
+  if (finished) {
+    update[browser + '-pending'] = {Value: {N: '0'}};
+    update.pending = {Value: {N: '-1'}, Action: 'ADD'};
+  }
   return fix(db.updateItem({
-    TableName: 'builds',
-    Key: {
-      HashKeyElement: {S: user + '/' + repo},
-      RangeKeyElement: {S: tag}
-    },
-    AttributeUpdates: update
-  }));
+      TableName: 'builds',
+      Key: {
+        HashKeyElement: {S: user + '/' + repo},
+        RangeKeyElement: {N: buildID}
+      },
+      AttributeUpdates: update,
+      Expected: expected,
+      ReturnValues: 'ALL_NEW'
+    }))
+    .then(function (res) {
+      return +res.Attributes.pending.N;
+    }, function (err) {
+      finished = err.name === 'AWS:ConditionalCheckFailedException';
+      if (err.name != 'AWS:ConditionalCheckFailedException')
+        throw err;
+      else
+        return getBuild(user, repo, buildID)
+          .then(function (res) {
+            return res.pending;
+          });
+    })
+    .then(function (pending) {
+      if (pending === 0) {
+        return completeBuild(user, repo, buildID);
+      }
+    })
+    .then(function () {
+      return finished;
+    });
 }
-
+var fail = false;
+function completeBuild(user, repo, buildID) {
+  if (fail) return;
+  return fix(db.updateItem({
+      TableName: 'repositories',
+      Key: {
+        HashKeyElement: { S: user },
+        RangeKeyElement: { S: repo }
+      },
+      Expected: {
+        currentBuild: {Value: {N: '' + buildID}}
+      },
+      AttributeUpdates: {
+        latestBuild: {Value: {N: '' + buildID} }
+      }
+    }))
+    .then(function(){}, function (err) {
+      if (err.name != 'AWS:ConditionalCheckFailedException')
+        throw err;
+    });
+}
 var s3 = new aws.S3();
 //read from S3
 function getBuildReport(user, repo, tag, browser) {
